c5e86da75167e117598677714928451fc4456864
iqbalaamir <aamir.iqbal040@gmail.com>
2023-04-01 11:34:51 +0530

    books data

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const books = [
         "https://cdn.britannica.com/64/103064-050-295C6879/Charlottes-Web-EB-Garth-Williams.jpg",
       rating: 8.8,
       trailer: "https://www.youtube.com/embed/PU2r9tDwZ1M",
+      language: "English",
       summary:
         "The novel tells the story of a livestock pig named Wilbur and his friendship with a barn spider named Charlotte. When Wilbur is in danger of being slaughtered by the farmer, Charlotte writes messages praising Wilbur in her web in order to persuade the farmer to let him live.",
     },
@@ -23,6 +24,7 @@ const books = [
       poster: "https://miro.medium.com/max/1400/1*ItFOYfi8Dyy0yj9n1SE8uQ.jpeg",
       rating: 8.1,
       trailer: "https://www.youtube.com/embed/gqviJoSkf6U",
+      language: "English",
       summary:
         "Attitude, In psychology, a mental position with regard to a fact or state. Attitudes reflect a tendency to classify objects and events and to react to them with some consistency. Attitudes are not directly observable but rather are inferred from the objective, evaluative responses a person makes.",
     },
@@ -32,6 +34,7 @@ const books = [
       poster: "https://m.media-amazon.com/images/I/81fdQIY6ykL.jpg",
       rating: 8.8,
       trailer: "https://www.youtube.com/embed/san61qTwWsU",
+      language: "English",
       summary:
         "There's no secret to The Secret. The book and movie simply state that your thoughts control the universe. Through this “law of attraction” you “manifest” your desires. “It is exactly like placing an order from a catalogue",
     },
@@ -40,6 +43,7 @@ const books = [
       name: "Discover Your Destiny",
       poster: "https://m.media-amazon.com/images/I/61t18yWH5qL.jpg",
       rating: 6,
+      language: "Tamil",
       trailer: "https://www.youtube.com/embed/o8wUR2JAeUw",
       summary:
         "'Discover Your Destiny' is a story about enlightenment of Dar Sanderson, who is an incredibly ambitious executive. The book throws light on the fact that 'happiness and harmony can never be achieved and assured by SUCCESS'. Dar is an achiever in almost every aspect of life, yet he is void from the inside.",
@@ -50,6 +54,7 @@ const books = [
       poster: "https://m.media-amazon.com/images/I/71zytzrg6lL.jpg",
       rating: 8.6,
       trailer: "https://www.youtube.com/embed/Kxvp3eOYphY",
+      language: "English",
       summary:
         "In The 5 AM Club: Own Your Morning. Elevate Your Life, he uses a fictitious story about a billionaire mentor teaching a struggling artist and an entrepreneur about the importance of waking up early to show how revolutionary it is for success.",
     },
@@ -59,6 +64,7 @@ const books = [
       poster:
         "https://play-lh.googleusercontent.com/1aghoDaz52K3bbZA3EJGHvEpgaru4uMC3Ud2ik_EAW7SjNLwK7nXxOp_Uad-3L6Ovvg4C2-_d1kqVg=w480-h690-rw",
       rating: 9,
+      language: "Tamil",
       summary:
         "Louise expands on her philosophy of loving the self and shows you how to overcome emotional barriers through learning to listen to your inner voice, loving the child within, letting your true feelings out, and much more!",
       trailer: "https://www.youtube.com/embed/4UzY6ksC6gU",
@@ -71,6 +77,7 @@ const books = [
       summary: "elon musk: tesla, spacex, and the quest for a fantastic future",
       trailer: "elon musk: tesla, spacex, and the quest for a fantastic future",
       id: "007",
+      language: "English",
     },
     {
       name: "Harry potter",
@@ -81,6 +88,7 @@ const books = [
         "Adaptation of the first of J.K. Rowling's popular children's novels about Harry Potter, a boy who learns on his eleventh birthday that he is the orphaned son of two powerful wizards and possesses unique magical powers of his own.",
       trailer: "https://www.youtube.com/embed/fFGS4zZWGoA",
       id: "008",
+      language: "English",
     },
   ];
   
